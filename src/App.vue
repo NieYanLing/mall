90c26403274dd0cd9625c4bf6eef550159810e25
Nie <1358277662@qq.com>
2022-08-24 11:15:53 +0800

    提交代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-  
+    <div>{{message}}</div>
   </div>
 </template>
 <script>
 export default {
-  name:'App'
+  name:'App',
+  data(){
+    return{
+      message:'Hello world'
+    }
+  }
 }
 </script>
 <style>
